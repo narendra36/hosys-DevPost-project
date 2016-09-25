39d12655a561709064bc4da4505d004093e41918
Narendra-Dodwaria <dodwaria.nd536@gmail.com>
2016-09-25 21:06:09 +0530

    nexttry2

--- doctorList.php
+++ doctorList.php
@@ -48,7 +48,9 @@
 		$username = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
 		$password = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
 		$dbname = getenv("OPENSHIFT_MYSQL_DB_NAME");
-		$conn = new mysqli($servername, $username, $password, $dbname);
+		echo "hii narendra ! ";
+		echo "servername : ".$servername." ,  username : ".$username." , password : ".$password." , dbname : ".$dbname;
+		/*$conn = new mysqli($servername, $username, $password, $dbname);
 		if ($conn->connect_error) {
 		    die("Connection failed Sorry yr: " . $conn->connect_error);
 		}
