ddb3945910f69a25d69b8ea34fe1dfd1656ee009
Narendra-Dodwaria <dodwaria.nd536@gmail.com>
2016-09-25 22:16:21 +0530

    nexttry3

--- doctorList.php
+++ doctorList.php
@@ -45,12 +45,12 @@
 	</div>
 	<?php
 		
-		$dbhost = getenv("MYSQL_SERVICE_HOST");
-		$dbport = getenv("MYSQL_SERVICE_PORT");
-		$dbuser = getenv("MYSQL_USER");
-		$dbpwd = getenv("MYSQL_PASSWORD");
-		$dbname = getenv("MYSQL_DATABASE");
-		echo $dbhost." , ".$dbhost." , ".$dbuser." , ".$dbpwd." , ".$dbname;
+		$dbhost = getenv("OPENSHIFT_MYSQL_DB_HOST");
+		$dbport = getenv("OPENSHIFT_MYSQL_DB_PORT");
+		$dbuser = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+		$dbpwd = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+		$dbname = getenv("OPENSHIFT_MYSQL_DB_URL");
+		echo $dbhost." , ".$dbport." , ".$dbuser." , ".$dbpwd." , ".$dbname;
 		/*$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 		if ($connection->connect_errno) {
 		    printf("Connect failed: %s\n", $mysqli->connect_error);
